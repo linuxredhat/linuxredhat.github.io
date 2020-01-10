4fa0e0bf281859fa0515a38b3ca53fabd2e9b075
renjin <renjin@huatu.com>
2020-01-10 16:57:55 +0800

    hexo

--- live2d-widget/waifu-tips.js
+++ live2d-widget/waifu-tips.js
@@ -8,7 +8,7 @@ function loadWidget(waifuPath, apiPath) {
 	sessionStorage.removeItem("waifu-text");
 	document.body.insertAdjacentHTML("beforeend", `<div id="waifu">
 			<div id="waifu-tips"></div>
-			<canvas id="live2d" width="300" height="300"></canvas>
+			<canvas id="live2d" width="200" height="200"></canvas>
 			<div id="waifu-tool">
 				<span class="fa fa-lg fa-comment"></span>
 				<span class="fa fa-lg fa-paper-plane"></span>
